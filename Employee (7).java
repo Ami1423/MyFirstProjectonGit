d0c598ac5c5dbc9f0253b30bd2015a82e68815ee
Ami1423 <63359009+Ami1423@users.noreply.github.com>
2020-04-08 12:15:20 -0400

    This is a important file
    kldkgsdfj

---
+++ Employee (7).java
@@ -0,0 +1,47 @@
+package com.db.SpringJPAApplication;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	private int id;
+	
+	
+	private String name;
+	private double salary;
+	private String manager;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getManager() {
+		return manager;
+	}
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+	
+	
+	
+
+}
